b155e973a037b64bdb21b3e467763a9a78e609d0
Noemí Saavedra <djnoe.np@gmail.com>
2020-11-15 16:37:19 -0400

    personal

---
+++ ejemplo01/Principal.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "NOMBRE COMPLETO: Noemi Saavedra Párraga" << endl;
+	cout << "CARRERA: Ingenieria de Sistemas" << endl;
+	cout << "C.U.: 35-4978" << endl;
+	system("pause");
+	return 0;
+}
